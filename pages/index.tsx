c862472a1628b5fe79371e9684abdb6920708e6c
unknown <akhilrajktt@gmail.com>
2022-02-02 11:31:14 +0530

    corrections

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,8 +22,8 @@ export default function Home() {
       <main className="xl:overflow-hidden relative" id="home">
         <div className="flex flex-col xl:flex-row gap-5 max-w-screen-xl mx-auto">
           <div className="p-5 pb-20 py-24 xl:py-20 flex-1 max-w-xl xl:max-w-none mx-auto">
-            <div className="font-readex text-4xl text-center xl:text-left md:text-5xl font-bold">Hi, I&#39;m Akhil Raj and I&#39;am a <span className="underline text-indigo-500">Web developer</span> & Freelancer.</div>
-            <div className="font-readex text-center xl:text-left pt-5 sm:text-lg text-gray-800">I am a web developer based out of Chennai, India. I have 2 years of experience and i specialize in web development using stacks like React Js, Node Js, Next Js and much more. I am open for new opportunities and interesting projects.</div>
+            <div className="font-readex text-4xl text-center xl:text-left md:text-5xl font-bold">Hi, I&#39;m Akhil Raj and I&#39;m a <span className="underline text-indigo-500">Web developer</span> & Freelancer.</div>
+            <div className="font-readex text-center xl:text-left pt-5 sm:text-lg text-gray-800">I am a web developer based out of Chennai, India. I have 2 years of experience and I specialize in web development using stacks like React Js, Node JS, Next JS, and much more. I am open to new opportunities and interesting projects.</div>
             <div className="pt-5 flex justify-center xl:justify-start">
               <Link1 to="connect" spy={true} smooth={true} className="bg-white text-center cursor-pointer hover:scale-105 active:scale-95 duration-150 py-2 font-semibold px-4 rounded-md w-full md:w-max border-2 border-black font-readex">Let&#39;s Talk</Link1>
             </div>
