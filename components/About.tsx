70354c52aab2f5eb5312c6c02d0684e019c5cbe6
unknown <akhilrajktt@gmail.com>
2022-02-02 11:35:07 +0530

    corrections

--- components/About.tsx
+++ components/About.tsx
@@ -17,7 +17,7 @@ function About() {
               <div className="flex-1">
                   <div className="text-sm pb-1 text-center md:text-left xl:text-lg text-blue-800">{aboutText}</div>
                   <div className="text-4xl xl:text-5xl text-center md:text-left font-bold">Developing is my passion.</div>
-                  <div className="sm:text-base xl:text-lg md:max-w-lg text-center md:text-left py-5">A self taught developer interested in developing as well as learning new technologies. I started my development journey as a freelancer which helped me learn a lot.<br/> I started aquiring the projects first, then figured out the technologies needed to complete the project, this helped me in learning new technologies.</div>
+                  <div className="sm:text-base xl:text-lg md:max-w-lg text-center md:text-left py-5">A self-taught developer, interested in developing as well as learning new technologies. I started my development journey as a freelancer which helped me learn a lot. I started acquiring the projects first, then figured out the technologies needed to complete the project, this helped me in learning new technologies.</div>
               </div>
               {/* image */}
               <div className="flex-1 bg-gray-800 rounded-lg overflow-hidden">
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -8,7 +8,7 @@ function Skills() {
           {/* HTML CSS JS React Next Tailwind scss Node js mongodb firebase */}
           <div className="text-4xl xl:text-5xl text-center font-bold">Skills</div>
           <div>
-              <p className="sm:text-base xl:text-lg  text-center py-5">My portfolio was developed using <span className="border-b-2 border-gray-900 font-semibold">Next Js</span> and <span className="border-b-2 font-semibold border-cyan-500 text-cyan-500">Tailwind css</span>. <br/><span className="text-gray-500">Its all code, no Images used - other than mine*.</span> </p>
+              <p className="sm:text-base xl:text-lg  text-center py-5">My portfolio was developed using <span className="border-b-2 border-gray-900 font-semibold">Next Js</span> and <span className="border-b-2 font-semibold border-cyan-500 text-cyan-500">Tailwind css</span>. <br/><span className="text-gray-500">Its all code, no Images used - other than mine and my work designs*.</span> </p>
           </div>
           {/* dashboard and skills */}
           <div>
